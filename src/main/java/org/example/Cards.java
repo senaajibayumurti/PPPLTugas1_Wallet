0d8ce347cfa581ed9400890367c5ad403e4ea8e3
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-02-29 13:02:53 +0700

    feat: Card dan polymorphism COnstructornya

--- src/main/java/org/example/Cards.java
+++ src/main/java/org/example/Cards.java
@@ -3,25 +3,14 @@ package org.example;
 public class Cards {
     private String cardName;
     private double balance;
-    private boolean active;
 
     public Cards(String cardName) {
         this.cardName = cardName;
-        this.active = true;
     }
 
     public Cards(String cardName, double balance) {
         this.cardName = cardName;
         this.balance = balance;
-        this.active = true;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
     }
 
     public String getCardName() {
